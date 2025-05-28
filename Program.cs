060231067e0f283be1940032b21a5012cc8be2ff
dayul <dyulchoo@naver.com>
2025-05-28 10:10:15 +0900

    오버로딩 주의점

--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@ namespace PropExample
     {
         static void Main(string[] args)
         {
+            TestOver(3);
         }
+
+        // 메서드 오버로딩
+        public static int TestOver(int input) { return 0; }
+        //public bool TestOver(int input) { return true;  }   // 동일한 매개변수 안됨
+        public static bool TestOver(double input) { return true; }
     }
 }
